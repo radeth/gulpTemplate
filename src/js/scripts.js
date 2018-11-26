15d35438686ec40d0ebcccd03e2a08abdd3ee1c8
radeth <123radeth@gmail.com>
2018-11-26 22:45:23 +0100

    first commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,14 @@
 'use strict';
+(function () {
+    AOS.init();
+    $(".buttonMore").on('click', function () {
+        $('html,body').animate({
+            scrollTop: $("section").offset().top
+        }, 1000)
+    })
+    $('.content').on('focus', function () {
+        console.log('test')
+    })
+
+})();
+
